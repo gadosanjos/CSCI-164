58decb9259c9b434afb4373c48c21c54fa41a7a0
Gabriel <147287432+gadosanjos@users.noreply.github.com>
2024-06-16 16:21:49 -0700

    updated to better reflect book content

--- chapter1/1.1/pandas_example.py
+++ chapter1/1.1/pandas_example.py
@@ -8,3 +8,11 @@ mydataset = {
 myvar = pd.DataFrame(mydataset)
 
 print(myvar)
+
+""" 
+$ python chapter1/1.1/pandas_example.py
+    cars  passings
+0    BMW         3
+1  Volvo         7
+2   Ford         2 
+"""
